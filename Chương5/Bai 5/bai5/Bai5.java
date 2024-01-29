d237480d4d17ab7887924f93b75a9d16197b8475
Van-HTr <138645786+Van-HTr@users.noreply.github.com>
2024-01-29 20:18:17 +0700

    Add files via upload
    Trần Hồng Vân

---
+++ Chương5/Bai 5/bai5/Bai5.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bai5;
+
+/**
+ *
+ * @author Dell
+ */
+public class Bai5 {
+    public static void main(String[] args) {
+        Student st = new Student();
+    }
+}
